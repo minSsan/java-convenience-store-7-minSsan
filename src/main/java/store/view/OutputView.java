b923dd539e5466650672e406319ccb9aef3b3983
minSsan <msp1125@hanyang.ac.kr>
2024-11-08 16:28:59 +0900

    feat(OutputView): 프로그래밍 요구사항 outputview 뼈대 코드 작성

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,15 @@
+package store.view;
+
+public class OutputView {
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printWelcome() {
+        System.out.println("안녕하세요. W편의점입니다.");
+    }
+
+    public void printProducts() {
+        System.out.println("- 콜라 1,000원 10개 탄산2+1");
+    }
+}
