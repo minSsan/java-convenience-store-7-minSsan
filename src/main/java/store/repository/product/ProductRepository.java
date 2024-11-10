5aacdc1dfb08edfed36f5f081f7aa460dcd05021
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 03:23:36 +0900

    fix(ProductRepository): 메소드 추가사항 커밋 누락

--- src/main/java/store/repository/product/ProductRepository.java
+++ src/main/java/store/repository/product/ProductRepository.java
@@ -2,7 +2,10 @@ package store.repository.product;
 
 import store.domain.vo.Product;
 import store.domain.vo.Name;
+import store.domain.Promotion;
 
 public interface ProductRepository {
+    Promotion findPromotionByName(Name name);
     Product findByName(Name name);
+    void save(Product product, Promotion promotion);
 }
--- src/main/java/store/repository/product/ProductRepositoryImpl.java
+++ src/main/java/store/repository/product/ProductRepositoryImpl.java
@@ -10,10 +10,24 @@ public class ProductRepositoryImpl implements ProductRepository {
     private final HashMap<Product, Promotion> items = new HashMap<>();
 
     @Override
+    public Promotion findPromotionByName(Name name) {
+        Product product = this.findByName(name);
+        if (product == null) {
+            return null;
+        }
+        return items.get(product);
+    }
+
+    @Override
     public Product findByName(Name name) {
         return items.keySet().stream()
                 .filter(product -> product.name().equals(name))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public void save(Product product, Promotion promotion) {
+        items.put(product, promotion);
     }
 }
