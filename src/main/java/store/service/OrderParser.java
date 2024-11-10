b42488edd8c4f86a85ba931c2f73812fa27d86d6
minSsan <msp1125@hanyang.ac.kr>
2024-11-10 21:43:07 +0900

    feat(OrderParser): 사용자 입력 파싱 클래스 추가

---
+++ src/main/java/store/service/OrderParser.java
@@ -0,0 +1,11 @@
+package store.service;
+
+import store.domain.vo.Order;
+
+import java.util.List;
+
+public class OrderParser {
+    public List<Order> from(String input) {
+        return null;
+    }
+}
