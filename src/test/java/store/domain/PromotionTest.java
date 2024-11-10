ca14f4a57d9955c762eca883597cb38cc2a80810
minSsan <msp1125@hanyang.ac.kr>
2024-11-10 22:53:51 +0900

    refactor: PromotionTest 경로 변경

--- src/test/java/store/domain/PromotionTest.java
+++ src/test/java/store/domain/PromotionTest.java
@@ -1,11 +1,12 @@
-package store.domain.vo;
+package store.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import store.domain.Promotion;
+import store.domain.vo.PromotionQueryResult;
+import store.domain.vo.Quantity;
 
 import java.time.LocalDate;
 
