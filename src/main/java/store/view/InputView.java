5884e3b2c7a808830b3e84a4fcad1f99c6d9e876
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 04:49:18 +0900

    feat(InputView): 예외 발생시 재입력 및 메시지 상수화

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,27 +1,30 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.infrastructure.constant.ExceptionMessage;
+import store.infrastructure.exception.CustomException;
 
 public class InputView {
+    private static final String YES = "Y";
+    private static final String NO = "N";
+
     public String readItem() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
 
     private boolean convertAnswerToBool(String answer) {
-        if (answer.trim().equals("Y")) {
+        if (answer.trim().equals(YES)) {
             return true;
         }
-        if (answer.trim().equals("N")) {
+        if (answer.trim().equals(NO)) {
             return false;
         }
-        throw new IllegalArgumentException("[ERROR] 입력 형식이 잘못되었습니다.");
+        throw new CustomException(ExceptionMessage.WRONG_INPUT_FORMAT.message());
     }
 
     public boolean readMembershipAnswer() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        String input = Console.readLine();
-        return convertAnswerToBool(input);
+        return readYesOrNoAnswer("멤버십 할인을 받으시겠습니까? (Y/N)");
     }
 
     /**
@@ -30,9 +33,7 @@ public class InputView {
      * @return
      */
     public boolean readAddFreeAnswer(String name) {
-        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", name);
-        String input = Console.readLine();
-        return convertAnswerToBool(input);
+        return readYesOrNoAnswer(String.format("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)", name));
     }
 
     /**
@@ -42,14 +43,24 @@ public class InputView {
      * @return
      */
     public boolean readPaidRegularPriceAnswer(String name, int count) {
-        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", name, count);
-        String input = Console.readLine();
-        return convertAnswerToBool(input);
+        return readYesOrNoAnswer(
+                String.format("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", name, count)
+        );
     }
 
     public boolean readAdditionalPurchase() {
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-        String input = Console.readLine();
-        return convertAnswerToBool(input);
+        return readYesOrNoAnswer("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+    }
+
+    private boolean readYesOrNoAnswer(String message) {
+        System.out.println(message);
+        while (true) {
+            String input = Console.readLine();
+            try {
+                return convertAnswerToBool(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
