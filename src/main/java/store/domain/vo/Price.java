2135ea499591cca81bfdf4ea8976300e0b1ce3cc
minSsan <msp1125@hanyang.ac.kr>
2024-11-10 14:56:02 +0900

    feat(Price): 정적 생성 메소드 추가 및 예외 메시지 상수화

--- src/main/java/store/domain/vo/Price.java
+++ src/main/java/store/domain/vo/Price.java
@@ -1,5 +1,8 @@
 package store.domain.vo;
 
+import store.infrastructure.constant.ExceptionMessage;
+import store.infrastructure.exception.CustomException;
+
 public record Price(int value) {
     public static final int MIN = 10;
     public static final int MAX = 1_000_000;
@@ -8,9 +11,17 @@ public record Price(int value) {
         validatePrice(value);
     }
 
+    public static Price from(String input) {
+        try {
+            return new Price(Integer.parseInt(input.trim()));
+        } catch (NumberFormatException e) {
+            throw new CustomException(ExceptionMessage.WRONG_INTEGER_FORMAT.message());
+        }
+    }
+
     private void validatePrice(int value) {
         if (value < MIN || value > MAX) {
-            throw new IllegalArgumentException("[ERROR] 상품 가격은 10 ~ 1,000,000 사이 숫자만 가능합니다.");
+            throw new CustomException(ExceptionMessage.WRONG_PRICE_RANGE.message());
         }
     }
 
--- src/main/java/store/infrastructure/constant/ExceptionMessage.java
+++ src/main/java/store/infrastructure/constant/ExceptionMessage.java
@@ -2,7 +2,8 @@ package store.infrastructure.constant;
 
 public enum ExceptionMessage {
     WRONG_INTEGER_FORMAT("숫자 형식이 올바르지 않습니다. 다시 입력해 주세요."),
-    WRONG_QUANTITY_RANGE("재고는 0 ~ 1000 사이의 숫자만 설정할 수 있습니다.")
+    WRONG_QUANTITY_RANGE("재고는 0 ~ 1000 사이의 숫자만 설정할 수 있습니다."),
+    WRONG_PRICE_RANGE("상품 가격은 10원 ~ 1,000,000원 사이의 숫자만 유효합니다."),
     ;
 
     private final String message;
