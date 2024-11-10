d9becb658168f6d010345b186585378cf65b5433
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 04:38:32 +0900

    feat(PromotionStrategyProvider): 전략 제공자 클래스 추가

---
+++ src/main/java/store/service/strategy/provider/PromotionStrategyProvider.java
@@ -0,0 +1,32 @@
+package store.service.strategy.provider;
+
+import store.domain.vo.PromotionOption;
+import store.service.strategy.AddFreePromotionStrategy;
+import store.service.strategy.ExcludeNonApplicablePromotionStrategy;
+import store.service.strategy.ImmutableOrderPromotionStrategy;
+import store.service.strategy.PromotionStrategy;
+
+public enum PromotionStrategyProvider {
+    ADD_FREE_AGREE(PromotionOption.ADD_FREE, true, new AddFreePromotionStrategy()),
+    ADD_FREE_DISAGREE(PromotionOption.ADD_FREE, false, new ImmutableOrderPromotionStrategy()),
+    REGULAR_PURCHASE_AGREE(PromotionOption.REGULAR_PURCHASE, true, new ImmutableOrderPromotionStrategy()),
+    REGULAR_PURCHASE_DISAGREE(
+            PromotionOption.REGULAR_PURCHASE,
+            false,
+            new ExcludeNonApplicablePromotionStrategy()
+    );
+
+    private final PromotionOption option;
+    private final boolean answer;
+    private final PromotionStrategy strategy;
+
+    private PromotionStrategyProvider(PromotionOption option, boolean answer, PromotionStrategy strategy) {
+        this.option = option;
+        this.answer = answer;
+        this.strategy = strategy;
+    }
+
+    public static PromotionStrategy from(PromotionOption option, boolean answer) {
+        return null;
+    }
+}
