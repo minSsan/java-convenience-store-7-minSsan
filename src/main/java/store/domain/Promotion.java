8a0954796d263ffef5bf9e86e0884cbd81422f1a
minSsan <msp1125@hanyang.ac.kr>
2024-11-10 21:01:53 +0900

    feat(Promotion): 테스트 통과 코드 작성

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,21 +1,63 @@
 package store.domain;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import store.domain.vo.*;
+import store.infrastructure.constant.ExceptionMessage;
 
 import java.time.LocalDate;
 
 public record Promotion(Name name, Quantity buyQuantity, Quantity getQuantity, LocalDate start, LocalDate end) {
     public static final int GET = 1;
 
+    public Promotion {
+        validateGet(getQuantity.value());
+        validateDate(start, end);
+    }
+
     public static Promotion of(String name, int buy, int get, LocalDate start, LocalDate end) {
         return new Promotion(new Name(name), new Quantity(buy), new Quantity(get), start, end);
     }
 
+    private void validateGet(int get) {
+        if (get != GET) {
+            throw new IllegalStateException(ExceptionMessage.WRONG_PROMOTION_GET_COUNT.message());
+        }
+    }
+
+    private void validateDate(LocalDate start, LocalDate end) {
+        if (start.isAfter(end)) {
+            throw new IllegalStateException(ExceptionMessage.WRONG_PROMOTION_DATE.message());
+        }
+    }
+
     public boolean isApplicable() {
-        return false;
+        LocalDate now = DateTimes.now().toLocalDate();
+
+        final boolean isAfterOrOnStart = start.isEqual(now) || start.isBefore(now);
+        final boolean isBeforeOrOnEnd = end.isEqual(now) || end.isAfter(now);
+
+        return isAfterOrOnStart && isBeforeOrOnEnd;
     }
 
     public PromotionQueryResult getQueryResult(Quantity quantity) {
-        return null;
+        Quantity giftedQuantity = getGiftedQuantity(quantity);
+        Quantity appliedQuantity = getAppliedQuantity(giftedQuantity);
+        Quantity notAppliedQuantity = quantity.subtract(appliedQuantity);
+        return new PromotionQueryResult(giftedQuantity, appliedQuantity, notAppliedQuantity);
+    }
+
+    private Quantity getGiftedQuantity(Quantity quantity) {
+        if (!this.isApplicable()) {
+            return Quantity.ZERO;
+        }
+        final int promotionUnitCount = buyQuantity.sum(getQuantity).value();
+
+        final int appliedCount = quantity.value() / promotionUnitCount;
+        return new Quantity(appliedCount);
+    }
+
+    private Quantity getAppliedQuantity(Quantity giftQuantity) {
+        final int promotionUnitCount = buyQuantity.sum(getQuantity).value();
+        return new Quantity(giftQuantity.value() * promotionUnitCount);
     }
 }
--- src/main/java/store/infrastructure/constant/ExceptionMessage.java
+++ src/main/java/store/infrastructure/constant/ExceptionMessage.java
@@ -1,9 +1,16 @@
 package store.infrastructure.constant;
 
+import store.domain.vo.Price;
+import store.domain.Promotion;
+import store.domain.vo.Quantity;
+
 public enum ExceptionMessage {
     WRONG_INTEGER_FORMAT("숫자 형식이 올바르지 않습니다. 다시 입력해 주세요."),
-    WRONG_QUANTITY_RANGE("재고는 0 ~ 1000 사이의 숫자만 설정할 수 있습니다."),
-    WRONG_PRICE_RANGE("상품 가격은 10원 ~ 1,000,000원 사이의 숫자만 유효합니다."),
+    WRONG_QUANTITY_RANGE(String.format("재고는 %,d ~ %,d 사이의 숫자만 설정할 수 있습니다.", Quantity.MIN, Quantity.MAX)),
+    WRONG_PRICE_RANGE(String.format("상품 가격은 %,d원 ~ %,d원 사이의 숫자만 유효합니다.", Price.MIN, Price.MAX)),
+    WRONG_PROMOTION_DATE("프로모션 기간이 올바르게 설정되지 않았습니다."),
+    WRONG_PROMOTION_GET_COUNT(String.format("현재 프로모션의 추가 상품은 %,d개만 가능합니다.", Promotion.GET)),
+    EXCEED_ORDER_COUNT("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     ;
 
     private final String message;
