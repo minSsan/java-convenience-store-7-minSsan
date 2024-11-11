8e5de260e2c637c69925d68c6f2f36ceda44234b
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 13:58:34 +0900

    fix(Product): equals 가격 비교 연산

--- src/main/java/store/domain/vo/Product.java
+++ src/main/java/store/domain/vo/Product.java
@@ -4,7 +4,7 @@ public record Product(Name name, Price price) {
     @Override
     public boolean equals(Object o) {
         if (o instanceof Product) {
-            return ((Product) o).name().equals(name) && ((Product) o).price() == price;
+            return ((Product) o).name().equals(name) && ((Product) o).price().equals(price);
         }
         return false;
     }
