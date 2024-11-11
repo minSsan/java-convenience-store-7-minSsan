c9c7ef369c7496e833e311264b9e57b6c2e834b4
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 12:57:50 +0900

    feat(DiscountInfo): 할인 정보를 관리하는 도메인 객체 추가

---
+++ src/main/java/store/domain/DiscountInfo.java
@@ -0,0 +1,57 @@
+package store.domain;
+
+import store.domain.vo.Product;
+import store.domain.vo.Quantity;
+import store.infrastructure.constant.Membership;
+
+import java.util.HashMap;
+
+public class DiscountInfo {
+    private final HashMap<Product, Quantity> promotion;
+    private Integer membership;
+
+    public DiscountInfo() {
+        promotion = new HashMap<>();
+        membership = 0;
+    }
+
+    public void reset() {
+        promotion.clear();
+        membership = 0;
+    }
+
+    public void addPromotionDiscount(Product product, Quantity quantity) {
+        if (promotion.get(product) == null) {
+            promotion.put(product, quantity);
+            return;
+        }
+        promotion.put(product, promotion.get(product).sum(quantity));
+    }
+
+    public void setMembershipDiscount(int total) {
+        this.membership = Math.min(total * Membership.DISCOUNT_RATE / 100, Membership.MAX);
+    }
+
+    public int getPromotionCount(Product product) {
+        if (promotion.get(product) == null) {
+            return 0;
+        }
+        return promotion.get(product).value();
+    }
+
+    public int getTotalPromotionDiscount() {
+        int total = 0;
+        for (Product product : promotion.keySet()) {
+            total += product.price().value() * promotion.get(product).value();
+        }
+        return total;
+    }
+
+    public HashMap<Product, Quantity> getPromotionResult() {
+        return promotion;
+    }
+
+    public int getMembershipDiscount() {
+        return this.membership;
+    }
+}
---
+++ src/main/java/store/infrastructure/constant/Membership.java
@@ -0,0 +1,6 @@
+package store.infrastructure.constant;
+
+public class Membership {
+    public static final int DISCOUNT_RATE = 30;
+    public static final int MAX = 8_000;
+}
