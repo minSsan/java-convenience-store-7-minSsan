85349be71c66e58f25e449a325db7f8c3b2a0819
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 23:46:35 +0900

    style(PromotionStrategy): 주석 삭제

--- src/main/java/store/service/strategy/PromotionStrategy.java
+++ src/main/java/store/service/strategy/PromotionStrategy.java
@@ -6,10 +6,5 @@ import store.domain.Promotion;
 import store.service.dto.response.PromotionCommandResponse;
 
 public interface PromotionStrategy {
-    /**
-     *
-     * @param order 주문 정보
-     * @return 프로모션 혜택 적용 수량
-     */
     PromotionCommandResponse apply(Order order, Inventory inventory, Promotion promotion);
 }
