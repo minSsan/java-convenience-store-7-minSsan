9f5408c03007755d216849b31f48c7d30af10afe
minSsan <msp1125@hanyang.ac.kr>
2024-11-11 13:00:15 +0900

    refactor(ProductInfo): 출력 책임 해제

--- src/main/java/store/domain/ProductInfo.java
+++ src/main/java/store/domain/ProductInfo.java
@@ -4,17 +4,4 @@ import store.domain.vo.Product;
 import store.domain.vo.Quantity;
 
 public record ProductInfo(Product product, Quantity quantity, Promotion promotion) {
-    @Override
-    public String toString() {
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append(String.format("%s %,d원 %s",
-                product.name().value(),
-                product.price().value(),
-                quantity.toString()
-        ));
-        if (promotion == null) {
-            return stringBuffer.toString();
-        }
-        return stringBuffer.append(String.format(" %s", promotion.name().value())).toString();
-    }
 }
